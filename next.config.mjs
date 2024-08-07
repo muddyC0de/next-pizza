879321946ba6f36d46f1a33dbf9f12af46a4ab35
Astralapse <vladlubcenko8@gmail.com>
2024-08-07 20:29:03 +0300

    fixedddd

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  env: { NEXTAUTH_URL: "https://next-pizza-flax-nine.vercel.app/" },
 };
 
 export default nextConfig;
