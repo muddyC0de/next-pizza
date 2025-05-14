2b9e1abf77777b1eb8abdb5071fc753e82cca799
Astralapse <vladlubcenko8@gmail.com>
2025-05-14 23:45:02 +0300

    fix api problems

--- lib/find-pizzas.ts
+++ lib/find-pizzas.ts
@@ -13,15 +13,19 @@ export interface GetSearchParams {
 const DEFAULT_MIN_PRICE = 0;
 const DEFAULT_MAX_PRICE = 1000;
 
-export const findPizzas = async (params: GetSearchParams) => {
-  const sizes = params.sizes?.split(",").map((item) => Number(item));
-  const pizzaTypes = params.pizzaTypes?.split(",").map((item) => Number(item));
-  const ingredientsIdArr = params.ingredients
+export const findPizzas = async (params: Promise<GetSearchParams>) => {
+  const sizes = await (await params).sizes
     ?.split(",")
     .map((item) => Number(item));
-  const priceFrom = Number(params.priceFrom) || DEFAULT_MIN_PRICE;
-  const priceTo = Number(params.priceTo) || DEFAULT_MAX_PRICE;
-  const sortBy = params.sortBy || "asc";
+  const pizzaTypes = (await params).pizzaTypes
+    ?.split(",")
+    .map((item) => Number(item));
+  const ingredientsIdArr = (await params).ingredients
+    ?.split(",")
+    .map((item) => Number(item));
+  const priceFrom = Number((await params).priceFrom) || DEFAULT_MIN_PRICE;
+  const priceTo = Number((await params).priceTo) || DEFAULT_MAX_PRICE;
+  const sortBy = (await params).sortBy || "asc";
   const categories = await prisma.category.findMany({
     include: {
       products: {
